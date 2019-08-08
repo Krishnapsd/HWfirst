df91374eb168d7186219b7601b05708344a8240b
krishnapsd.thapaliya@gmail.com <krishnapsd.thapaliya@gmail.com>
2019-08-08 14:37:17 -0500

    Commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int firstNo;
+                int secondno;
+                int sumVal;
+                int subVal;
+                int multVal;
+                double divVal;
+                int remVal;
+
+                Console.WriteLine("Enter Your Number: ");
+                firstNo = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Your Second Number: ");
+                secondno = Convert.ToInt32(Console.ReadLine());
+                sumVal = firstNo + secondno;
+                Console.WriteLine("Addition is: " + sumVal);
+                subVal = firstNo - secondno;
+                Console.WriteLine("Substraction is: " + subVal);
+                multVal = firstNo * secondno;
+                Console.WriteLine("Multiply is: " + multVal);
+                divVal = (double)firstNo / secondno;
+                Console.WriteLine("Division is: " + divVal);
+                remVal = firstNo % secondno;
+                Console.WriteLine("Remainder is: " + remVal);
+                Console.ReadLine();
+
+            
+
+        }
+    }
+}
+
